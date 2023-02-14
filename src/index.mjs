d32942d744b3c0c380316f0e47f720516bbed158
Mourad RL CADI <mouradface1998@gmail.com>
2023-02-14 16:00:38 +0100

    save

--- src/index.mjs
+++ src/index.mjs
@@ -1,10 +1,33 @@
 import puppeteer from "puppeteer";
+import crypto from "crypto-js";
+import convert from "xml-js";
+import trans from "cyrillic-to-translit-js";
+import fs from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const log = console.log;
 
 const TARGET = "https://crm.metriks.ru/shahmatki/agent";
 
 const TIMEOUT = 1000 * 60;
+
+const now = () => (Date.now() / 1000).toFixed(0);
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(() => resolve(void 0), ms));
+
+const writeXML = (complexes) => {
+  let result = convert.js2xml({ complexes }, { compact: true, ignoreComment: true, spaces: 4 });
+
+  // prettier-ignore
+  fs.writeFileSync(
+      __dirname + '/../xml/metrics:' + trans().transform(complexes.complex.name, '-').toLowerCase() + '.xml',
+      '<?xml version="1.0" encoding="UTF-8" ?>\n' + result,
+      { flag: 'wx' }
+  );
+};
 
 const run = async () => {
   const browser = await puppeteer.launch();
@@ -17,108 +40,123 @@ const run = async () => {
 
   /* const complexes = await page.$$eval(".main__item", (els) => els.map((v) => v)); */
 
-  log("Generating complex details: name, homes[links]");
-  const complexes = await page.evaluate(() => {
-    const container_class = ".main__item";
+  log("Generating complex details: name, homes_links");
 
+  const complexes_meta = await page.evaluate(() => {
+    const container_class = ".main__item";
     return Array.from(document.querySelectorAll(container_class)).map((el) => ({
-      id: "",
       name: el.querySelector(`${container_class + "-top"} ${container_class + "-name"}`).textContent,
-      buildings_details: Array.from(el.querySelectorAll("a")).map((anchor) => ({ link: anchor.href, name: anchor.innerText, id: "" })),
+      buildings_details: Array.from(el.querySelectorAll("a")).map((anchor) => ({
+        name: anchor.innerText,
+        link: anchor.href,
+      })),
     }));
   });
 
-  const _complexes = [];
-
-  log("Looping through complexes");
-  for (let { name: complex_name, buildings_details, id: complex_id } of complexes) {
+  for (let { name: complex_name, buildings_details } of complexes_meta) {
     const _buildings = [];
 
-    log("Working on complex %s", complex_name);
-    for (let { link, name, id } of buildings_details) {
+    log("[Working on complex] %s", complex_name);
+
+    for (let { link, name } of buildings_details) {
+      log("[Working building] in %s", name);
+
       await page.goto(link);
 
-      log("[LOADING PAGE] %s", link);
+      log("[LOADING BUILDING] %s", link);
 
       const entrance_selectors = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll(".chess__porch-item")).map((e) => `${".chess__porch-item"}[data-id="${e.dataset.id}"]`);
+        const _selector = ".chess__porch-item";
+        return Array.from(document.querySelectorAll(_selector)).map((e) => `${_selector}[data-id="${e.dataset.id}"]`);
       });
 
-      const _apartments = [];
+      const _flats = [];
 
-      log("Working on a single entrance in %s", name);
       for (let _selector of entrance_selectors) {
         try {
           // prettier-ignore
           /* const [response] =  */ await Promise.all([
           page.waitForNavigation({waitUntil: 'networkidle0', timeout: TIMEOUT}),
           page.click(_selector),
-          /* page.waitForSelector(".chess__item.white", { timeout: TIMEOUT }) */
         ]);
-        } catch (e) {
-          log(e);
-          process.exit(0);
-        }
 
-        const apartments_selectors = await page.evaluate(() => {
-          return Array.from(document.querySelectorAll(".chess__item.white")).map((e) => `${".chess__item.white"}[data-id="${e.dataset.id}"]`);
-        });
+          const apartments_links = await page.evaluate(() => Array.from(document.querySelectorAll(".chess__href:has(> .white)")).map((a) => a.href));
 
-        log("Collecting apartments in the current entrance");
-        for (let apartment_selector of apartments_selectors) {
-          try {
-            // prettier-ignore
-            /* const [response] =  */ await Promise.all([
-            page.waitForNavigation({waitUntil: 'networkidle0', timeout: TIMEOUT}),
-            page.click(apartment_selector),
-            /* page.waitForSelector("#change-popup", { timeout: TIMEOUT }) */
-          ]);
-          } catch (e) {
-            log(e);
-            process.exit(0);
+          log("[Collecting apartments] in the current entrance");
+          for (let apartment_link of apartments_links) {
+            await page.goto(apartment_link);
+
+            await page.waitForNavigation({ waitUntil: "networkidle0", timeout: TIMEOUT });
+
+            await sleep(1000);
+
+            try {
+              /* await page.screenshot({ path: "screen.png", fullPage: true }); */
+              /* process.exit(1); */
+
+              const flat = await page.evaluate(() => {
+                return {
+                  apartment: document.getElementById("VIEW-NUM")?.innerText,
+                  is_apartment: document.getElementById("VIEW-TYPETEXT")?.innerText === "Квартира",
+                  count_rooms: document.getElementById("VIEW-ROOM")?.innerText,
+                  total_area: document.getElementById("VIEW-AREA")?.innerText,
+                  living_area: document.getElementById("VIEW-LIVING_AREA")?.innerText,
+                  kitchen_area: document.getElementById("VIEW-CITCHEN_AREA")?.innerText,
+                  layout_plan: document.querySelector(".layout__img img")?.src,
+                  deadline: document.getElementById("VIEW-CONSTRUCTION_YEAR")?.innerText,
+                  floor: document.getElementById("VIEW-FLOOR")?.innerText,
+                };
+              });
+
+              if (flat.is_apartment) {
+                delete flat.is_apartment;
+                _flats.push(flat);
+              }
+
+              log("Apartment %s extracted", flat.apartment);
+            } catch (error) {
+              log("Could not work on apartment %s", apartment_link);
+              log("///////////////////////////////////////////////////////////////////");
+              log("[ERROR]: ", error?.message);
+              log("///////////////////////////////////////////////////////////////////\n");
+              continue;
+            } finally {
+              await page.click(".edit__close");
+            }
           }
-
-          const apartment = await page.evaluate(() => ({
-            is_apartment: document.getElementById("VIEW-TYPETEXT").innerText === "Квартира",
-            num: document.getElementById("VIEW-NUM").innerText,
-            count_rooms: document.getElementById("VIEW-ROOM").innerText,
-            total_area: document.getElementById("VIEW-AREA").innerText,
-            living_area: document.getElementById("VIEW-LIVING_AREA").innerText,
-            kitchen_area: document.getElementById("VIEW-CITCHEN_AREA").innerText,
-            layout_plan: document.querySelector(".layout__img img").src,
-            deadline: document.getElementById("VIEW-CONSTRUCTION_YEAR").innerText,
-            floor: document.getElementById("VIEW-FLOOR").innerText,
-          }));
-
-          log("---------------------------------------------------------------------------------------------------------------------------");
-          log(apartment);
-          log("---------------------------------------------------------------------------------------------------------------------------");
-
-          /* process.exit(1); */
-
-          apartment.is_apartment && _apartments.push(apartment);
-
-          await page.click(".edit__close");
-
-          /* await page.screenshot({ path: "screen.png", fullPage: true }); */
+        } catch (error) {
+          log("Could not work on entrance %s", _selector);
+          log("///////////////////////////////////////////////////////////////////");
+          log("[ERROR]: ", error?.message);
+          log("///////////////////////////////////////////////////////////////////\n");
+          continue;
         }
       }
 
       _buildings.push({
-        apartments: _apartments,
+        id: crypto.MD5(name).toString(),
         name,
-        id,
+        flats: {
+          flat: _flats,
+        },
       });
     }
 
-    _complexes.push({
-      id: complex_id,
-      name: complex_name,
-      buildings: _buildings,
+    writeXML({
+      _attributes: {
+        timestamp: now(),
+      },
+      complex: {
+        id: crypto.MD5(complex_name).toString(),
+        name: complex_name,
+        buildings: { building: _buildings },
+      },
     });
-  }
 
-  log(_complexes);
+    log("//////////////////////////////////////////////////");
+    log("[Working On Complex Finished] %s", complex_name);
+    log("//////////////////////////////////////////////////");
+  }
 
   await browser.close();
 };
